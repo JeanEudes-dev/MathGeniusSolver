0907ea102fc07cdbff3ab9e1a1bf6047f09aabcb
JeanEudes-dev <jeudesmails@gmail.com>
2024-01-30 19:07:00 +0200

    Initial Django project setup

---
+++ mathsolver/differential_equation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DifferentialEquationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'differential_equation'
---
+++ mathsolver/numerical_methods/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NumericalMethodsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'numerical_methods'
